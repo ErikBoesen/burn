64ec7d30157592f4a563206b5c62f2fb8788c747
ErikBoesen <me@erikboesen.com>
2017-11-05 22:30:35 -0500

    Clean up script and update for how I now use my MacBook

--- setdown.sh
+++ setdown.sh
@@ -1,144 +1,102 @@
 #!/bin/bash
 
-# This script will wipe all suspicious material off my GM MacBook.
-# It will copy any important data to the SSH server aliased as "serv", whatever that is.
-# For now it will only work on boesene.
+# This script helps with cleaning up my MacBook. It automatically backs up needed
+# files to my SSH server (aliased as "serv") and removes anything private before
+# reimaging.
 
-# <!> WARNING!! <!>
-# DO NOT RUN THIS UNLESS YOU KNOW WHAT YOU ARE DOING.
-# YOU MAY LOSE FILES AND CONFIGURATION.
+# This script should be run as root.
 
-# This should be run from the /var/root directory and must be run as root.
+# Backup files over SSH, then remove, given paths
+function burm(){
+    (scp -r $@ serv:dump-$(hostname)/; rm -rf $@) &
+}
 
 if [ "$TERM" = "screen" ]; then
     echo "Running in screen or tmux. Will continue."
 else
-    echo "Please run this in screen (tmux is broken on our macs)."
-    exit
+    echo "Please run in screen or tmux."
+    exit 1
 fi
 
-echo "[WARNING] Will begin running in 5 seconds! Your terminal will be closed and your data will start disappearing!!"
+user=$(stat -f "%Su" /dev/console)
+uhm=/Users/$user
+
+echo "Burn beginning in 5 seconds..."
 sleep 5s
 
-echo "Removing this script from boesene's account before we start (will continue running regardless)..."
-rm -rf /Users/boesene/Desktop/setdown* &
-rm -rf /Users/boesene/setdown* &
+echo "Removing this script from user's account before we start..."
+rm -rf $uhm/setdown* /var/root/setdown* &
 
-echo "Removing this script from root..."
-rm -rf /var/root/set* &
+echo "Removing user's SSH known_hosts..."
+rm $uhm/.ssh/known_hosts*
 
-echo "Turning of SSHD for all users, which is off by default and could raise some red flags if noticed..."
+echo "Removing all dotfiles/folders in root dir, some of which maybe shouldn't be there..."
+rm -rf /var/root/.*
+
+echo "Getting user's ssh config so this all works more smoothly..."
+cp -r $uhm/.ssh /var/root/.ssh
+
+echo "Making dump dir on server..."
+ssh serv -t "mkdir -p ~/dump-$(hostname)"
+
+echo "Turning of SSHD for all users..."
 dscl . change /Groups/com.apple.access_ssh-disabled RecordName com.apple.access_ssh-disabled com.apple.access_ssh &
 
-echo "Giving /usr/local back to root..."
-chown -R root /usr/local &
+echo "Clearing cronjobs..."
+burm /var/at/tabs
 
-echo "Making an iTunes folder..."
-# To divert suspicion of having root. Obviously if they look in it they'll notice all the content is gone, but this will at least prevent them noticing something's up if for some reason they happen to ls /Applications.
-mkdir /Applications/iTunes.app
-
-echo "Removing Transmission..."
-rm -rf /Users/boesene/Documents/Transmission.app &
-
-echo "Removing all dotfiles/folders in root directory, some of which maybe shouldn't be there..."
-rm -rf /var/root/.*
-rm -rf /var/root/.ssh # This should already be removed, but let's do it again just in case. It NEEDS to be gone, or everything else gets screwed up (backups break).
-
-echo "Getting boesene's ssh config so this all works more smoothly..."
-cp -r /Users/boesene/.ssh /var/root/.ssh # Currently reconsidering all my life choices
-
-echo "Making dump directory on server..."
-# Makes a Desktop folder inside it just to save time later on
-ssh serv -t "mkdir -p ~/compdump/Desktop"
-
-echo "Backing up fish credentials folder..."
-scp -r /Users/boesene/creds serv:~/compdump/creds
-echo "Removing fish credentials folder..."
-rm -rf /Users/boesene/creds
-
-echo "Removing ~/bin..." # Some scripts, like sget, might raise some eyebrows
-rm -rf /Users/boesene/bin &
-
-echo "Clearing logs..."
-rm -rf /var/log/* &
+echo "Removing ~/bin..."
+rm -rf $uhm/bin &
 
 echo "Getting rid of all prompt histories..."
-rm /Users/*/.*history /var/root/.*history # root's should have been removed already, but just in case
+rm /Users/*/.*history /var/root/.*history
 
-echo "Removing fish if it's cloned..." # It's usually not, but if it was that could be very bad if found.
-rm -rf /Users/boesene/Desktop/fish &
-rm -rf /Users/boesene/fish &
+echo "Removing $user's zshrc..."
+burm $uhm/.zshrc
 
-echo "Removing boesene's zshrc..."
-rm /Users/boesene/.zshrc &
-
-echo "Backing up all files on Desktop which aren't updated on GitHub. (Repositories with uncommitted changes WILL be backed up.)"
-rm /Users/boesene/Desktop/notbackedup.txt
-touch /Users/boesene/Desktop/notbackedup.txt
-for directory in `ls /Users/boesene/Desktop`; do
-    cd "/Users/boesene/Desktop/$directory"
+echo "Backing up files in home directory..."
+touch $uhm/repos.txt
+for dir in $(ls $uhm); do
     # if file/folder either isn't a GitHub repository or has unpushed changes
     # This way we don't waste time copying a bunch of repos which are already on GitHub
-    if [ ! -d "/Users/boesene/Desktop/$directory/.git" ] || [[ `git status --porcelain` ]]; then
-        echo "- Copying $directory..."
-        scp -r "/Users/boesene/Desktop/$directory" "serv:~/compdump/Desktop/$directory" &
+    if [ -f $uhm/$dir ] || ! [ -e $uhm/$dir/.git ] || [[ $(git -C $uhm/$dir status --porcelain) ]]; then
+        burm $uhm/$dir
     else
-        echo $directory >> /Users/boesene/Desktop/notbackedup.txt
+        echo $dir >> $uhm/repos.txt
     fi
 done
-chmod 777 /Users/boesene/Desktop/notbackedup.txt
-scp /Users/boesene/Desktop/notbackedup.txt serv:~/compdump/Desktop/notbackedup.txt
-# A discovery of net would be a disaster, so get rid of that
-rm -rf /Users/boesene/Desktop/net
+burm $uhm/repos.txt
 
-echo "Disabling terminal session restoration..."
-# I myself already have this set to false.
-defaults write com.apple.Terminal NSQuitAlwaysKeepsWindows -bool false
+echo "Removing questionable repositories..."
+rm -rf $uhm/fish $uhm/net &
 
-echo "Removing .ssh again..."
-rm -rf /var/root/.ssh
-
-echo "Removing boesene's known_hosts (might contain root)..."
-rm /Users/boesene/.ssh/known_hosts*
-
-echo "Hiding evidence of PS proxying and/or Tor use. First, kill SSH to turn off PS proxy..."
-# We do this last so that screwing with the connection won't mess with SCP backups
-killall ssh
+echo "Clearing terminal backups..."
+rm -f $uhm/Library/Saved\ Application\ State/com.apple.Terminal.savedState/*
 
 echo "Wiping SOCKS proxy settings..."
-networksetup -setsocksfirewallproxy Wi-Fi "" "" # If you watch this in the SysPref GUI, it will actually put a 0 in the port field, but that will not be there next time you look at the GUI.
+networksetup -setsocksfirewallproxy Wi-Fi "" ""
 networksetup -setsocksfirewallproxystate Wi-Fi off
 
-echo "Uninstalling tor..."
-brew uninstall tor & # It takes a while to install, but around one second to remove
-rm /Users/boesene/torrc
-
-echo "Clearing logs again..."
+echo "Clearing logs..."
 rm -rf /var/log/*
 
-###########################################
-# Nothing past here is guaranteed to run. #
-# Don't put anything crucual under here.  #
-###########################################
+wait
 
-echo "Wipedown complete. Will kill all terminal sessions in three seconds."
+##############################################
+# Assume nothing past this message will run. #
+##############################################
+
+echo "Wipedown complete. Killing terminals."
 
 for i in 3 2 1; do
     echo "$i..."
     sleep 1s
 done
 
-echo "Killing terminal again to ease suspicions..."
+echo "Killing terminals..."
 
-clear;clear;clear;clear;clear;clear
+killall term Terminal ayy i_term iTerm2
 
-killall term
-killall Terminal
-
-echo "Killing all multiplex sessions... thanks for using setdown."
+echo "Killing all multiplex sessions..."
 killall tmux
 killall screen
-
-clear;clear;clear;clear;clear;clear
-
-exit
